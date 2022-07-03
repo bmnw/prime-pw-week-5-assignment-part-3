f8416f9459b7c5c7d2761a600a64e16c3fe801f8
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-07-02 21:19:59 -0500

    working on updating search function to include trackname searches

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -191,3 +191,81 @@ console.log('matches artist:', searchTwo({artist: 'Maggie Rogers'}));
 console.log('empty object test:', searchTwo({}));
 console.log('no input test:', searchTwo());
 
+// tracks updates to functions
+
+// addToCollectionUpdate
+
+/**
+ * Create a new album object with the input param as properties that is pushed into the collection [] and returns the new object
+ * @param {string} title Input title of album
+ * @param {string} artist Input album artist
+ * @param {number} yearPublished Input year of album release
+ * @param {[]} tracks Input array of album track object(s) with track name and duration
+ * @return new album object
+ */
+
+ function addToCollectionUpdate(title, artist, yearPublished, tracksArray){
+    console.log('in addToCollection:', title, artist, yearPublished);
+    const album = {
+        title: title,
+        artist: artist,
+        year: yearPublished,
+        tracks: tracksArray
+    };
+    console.log('new object:', album);
+    collection.push(album);
+    return album;
+};
+
+// addToCollectionUpdate test
+
+console.log('adding album with tracks', addToCollectionUpdate('Sainthood', 'Tegan and Sara', 2009, [{trackname: 'On Directing', duration: '2:46'}, {trackname: 'Arrow', duration: '3:07'}, {trackname: 'Sentimental Tune', duration: '3:23'}]));
+console.log(collection);
+console.log('adding album with tracks', addToCollectionUpdate('Hamilton: An American Musical', 'Lin-Manuel Miranda', 2015, [{trackname: 'Alexander Hamilton', duration: '3:57'}, {trackname: 'My Shot', duration: '5:33'}, {trackname: 'Wait For It', duration: '3:14'}]));
+console.log(collection);
+
+// searchUpdate
+
+/**
+ * Searches collection for albums that match the input object properties, including track name
+ * @param {object} searchInput Input search properties as an object
+ * @return Returns a new array containing albums in collection that match the input object properties, returns empty array if no matches, returns entire collection if input is empty.
+ */
+
+// album object properties: 
+// {artist, title, year}
+
+function searchUpdate(searchInput){
+    console.log('in search:', searchInput);
+    const resultsArray = [];
+    // in the following conditional, when I had (searchInput === undefined || searchInput === {}), it wouldn't push the collection if the input was {}. why not?
+    if(searchInput === undefined || Object.entries(searchInput).length === 0){
+        resultsArray.push(collection);
+        console.log('empty obj or no input results:', resultsArray);
+        return resultsArray;
+    } // end if input is empty
+    for(let i = 0; i<collection.length; i += 1){
+        if(searchInput.artist === collection[i].artist){
+            resultsArray.push(collection[i]);
+            console.log('artist match')
+        } // end checking if input artist has a match
+        if(searchInput.title === collection[i].title){
+            resultsArray.push(collection[i]);
+            console.log('title match');
+        } // end checking if input title has a match
+        if(searchInput.year === collection[i].year){
+            resultsArray.push(collection[i]);
+            console.log('year match');
+        } // end checking if input year has a match
+        if(searchInput.trackname === collection[i].tracks.trackname){
+            resultsArray.push(collection[i]);
+            console.log('trackname match');
+        }
+    }
+    console.log('search results:', resultsArray);
+    return resultsArray;
+}
+
+// searchUpdate test
+
+console.log('search by On Directing:', searchUpdate({trackname: 'On Directing'}));
